34a2d3e814f2640547a7242d259dc6ed3ee341d9
Aningwu Chisom <aningwuchisom@gmail.com>
2024-05-24 19:00:58 +0100

    added popup

--- frontend/src/Components/Category/Category.js
+++ frontend/src/Components/Category/Category.js
@@ -7,14 +7,27 @@ import Popup from "./Popup";
 import { useState } from "react";
 
 function Category() {
-  const [popupContent, setPopupContent] = useState(null);
+  const [popupState, setPopupState] = useState({
+    popup1: false,
+    popup2: false,
+    popup3: false,
+    popup4: false,
+    popup5: false,
+    popup6: false,
+  });
 
-  const showPopup = (content) => {
-    setPopupContent(content);
+  const showPopup = (popupName) => {
+    setPopupState((prevState) => ({
+      ...prevState,
+      [popupName]: true,
+    }));
   };
 
-  const closePopup = () => {
-    setPopupContent(null);
+  const closePopup = (popupName) => {
+    setPopupState((prevState) => ({
+      ...prevState,
+      [popupName]: false,
+    }));
   };
   return (
     <div className="category-container">
@@ -60,13 +73,13 @@ function Category() {
                 </div>
                 <button
                   className="the-hover-btn"
-                  onClick={() => showPopup("Content for Popup 1")}
+                  onClick={() => showPopup("popup1")}
                 >
                   Science
                 </button>
                 <Popup
-                  show={popupContent !== null}
-                  onClose={closePopup}
+                  show={popupState.popup1}
+                  onClose={() => closePopup("popup1")}
                 ></Popup>
                 <button className="the-hover-btn">Microbiology</button> <br />
                 <button className="the-hover-btn">MCB 206</button>
@@ -99,13 +112,13 @@ function Category() {
                 </div>
                 <button
                   className="the-hover-btn"
-                  onClick={() => showPopup("Content for Popup 1")}
+                  onClick={() => showPopup("popup2")}
                 >
                   Science
                 </button>
                 <Popup
-                  show={popupContent !== null}
-                  onClose={closePopup}
+                  show={popupState.popup2}
+                  onClose={() => closePopup("popup2")}
                 ></Popup>
                 <button className="the-hover-btn">Microbiology</button> <br />
                 <button className="the-hover-btn">MCB 206</button>
@@ -136,6 +149,94 @@ function Category() {
                   <h2 className="title">Essential Cell Biology</h2>
                   <span>Bruce Alberts</span>
                 </div>
+                <button
+                  className="the-hover-btn"
+                  onClick={() => showPopup("popup3")}
+                >
+                  Science
+                </button>
+                <Popup
+                  show={popupState.popup3}
+                  onClose={() => closePopup("popup3")}
+                ></Popup>
+                <button className="the-hover-btn">Microbiology</button> <br />
+                <button className="the-hover-btn">MCB 206</button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="category-books">
+              <div className="center-book">
+                <img
+                  style={{
+                    width: "120px",
+                    height: "150px",
+                    top: "44px",
+                    left: "66px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    display: "flex",
+                    border: "1px solid green",
+                  }}
+                  src={book3}
+                  alt="b00k1-pic"
+                />
+              </div>
+
+              <div>
+                <div className="title-container">
+                  <h2 className="title">Essential Cell Biology</h2>
+                  <span>Bruce Alberts</span>
+                </div>
+                <button
+                  className="the-hover-btn"
+                  onClick={() => showPopup("popup4")}
+                >
+                  Science
+                </button>
+                <Popup
+                  show={popupState.popup4}
+                  onClose={() => closePopup("popup4")}
+                ></Popup>
+                <button className="the-hover-btn">Microbiology</button> <br />
+                <button className="the-hover-btn">MCB 206</button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="category-books">
+              <div className="center-book">
+                <img
+                  style={{
+                    width: "120px",
+                    height: "150px",
+                    top: "44px",
+                    left: "66px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    display: "flex",
+                    border: "1px solid green",
+                  }}
+                  src={book3}
+                  alt="b00k1-pic"
+                />
+              </div>
+
+              <div>
+                <div className="title-container">
+                  <h2 className="title">Essential Cell Biology</h2>
+                  <span>Bruce Alberts</span>
+                </div>
+                <button
+                  className="the-hover-btn"
+                  onClick={() => showPopup("popup5")}
+                >
+                  Science
+                </button>
+                <Popup
+                  show={popupState.popup5}
+                  onClose={() => closePopup("popup5")}
+                ></Popup>
                 <button className="the-hover-btn">Science</button>
                 <button className="the-hover-btn">Microbiology</button> <br />
                 <button className="the-hover-btn">MCB 206</button>
@@ -166,66 +267,16 @@ function Category() {
                   <h2 className="title">Essential Cell Biology</h2>
                   <span>Bruce Alberts</span>
                 </div>
-                <button className="the-hover-btn">Science</button>
-                <button className="the-hover-btn">Microbiology</button> <br />
-                <button className="the-hover-btn">MCB 206</button>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="category-books">
-              <div className="center-book">
-                <img
-                  style={{
-                    width: "120px",
-                    height: "150px",
-                    top: "44px",
-                    left: "66px",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    display: "flex",
-                    border: "1px solid green",
-                  }}
-                  src={book3}
-                  alt="b00k1-pic"
-                />
-              </div>
-
-              <div>
-                <div className="title-container">
-                  <h2 className="title">Essential Cell Biology</h2>
-                  <span>Bruce Alberts</span>
-                </div>
-                <button className="the-hover-btn">Science</button>
-                <button className="the-hover-btn">Microbiology</button> <br />
-                <button className="the-hover-btn">MCB 206</button>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="category-books">
-              <div className="center-book">
-                <img
-                  style={{
-                    width: "120px",
-                    height: "150px",
-                    top: "44px",
-                    left: "66px",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    display: "flex",
-                    border: "1px solid green",
-                  }}
-                  src={book3}
-                  alt="b00k1-pic"
-                />
-              </div>
-
-              <div>
-                <div className="title-container">
-                  <h2 className="title">Essential Cell Biology</h2>
-                  <span>Bruce Alberts</span>
-                </div>
+                <button
+                  className="the-hover-btn"
+                  onClick={() => showPopup("popup6")}
+                >
+                  Science
+                </button>
+                <Popup
+                  show={popupState.popup6}
+                  onClose={() => closePopup("popup6")}
+                ></Popup>
                 <button className="the-hover-btn">Science</button>
                 <button className="the-hover-btn">Microbiology</button> <br />
                 <button className="the-hover-btn">MCB 206</button>
---
+++ frontend/src/Components/Library/Library.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./Library.css";
+import lib from "../../Images/lib.png";
+import lib2 from "../../Images/lib2.png";
+import lib3 from "../../Images/lib3.png";
+import lib4 from "../../Images/lib4.png";
+import filterbutton from "../../Images/Icons/filter-button.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+function Library() {
+  return (
+    <div className="library-container">
+      <div className="library-header" id="section2">
+        <h3 className="sub-head">Library</h3>
+        <button className="category-filter">
+          <img src={filterbutton} alt="icon" className="filter-icon" />
+          <span className="filter">Filter</span>
+        </button>
+      </div>
+
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={4}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+      >
+        <div className="library-row">
+          <SwiperSlide>
+            <div className="library">
+              <img
+                style={{
+                  width: "220px",
+                  height: "220px",
+                  boarderRaduis: "6px",
+                  boxShadow: "1px solid rgba(229, 230, 233, 1) ",
+                }}
+                src={lib}
+                alt="b00k1-pic"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="library">
+              <img
+                style={{
+                  width: "220px",
+                  height: "220px",
+                  boarderRaduis: "6px",
+                  boxShadow: "1px solid rgba(229, 230, 233, 1) ",
+                }}
+                src={lib2}
+                alt="b00k1-pic"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="library">
+              <img
+                style={{
+                  width: "220px",
+                  height: "220px",
+                  boarderRaduis: "6px",
+                  boxShadow: "1px solid rgba(229, 230, 233, 1) ",
+                }}
+                src={lib3}
+                alt="b00k1-pic"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="library">
+              <img
+                style={{
+                  width: "220px",
+                  height: "220px",
+                  boarderRaduis: "6px",
+                  boxShadow: "1px solid rgba(229, 230, 233, 1) ",
+                }}
+                src={lib4}
+                alt="b00k1-pic"
+              />
+            </div>
+          </SwiperSlide>
+        </div>
+      </Swiper>
+    </div>
+  );
+}
+
+export default Library;
